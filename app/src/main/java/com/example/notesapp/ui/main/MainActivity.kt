14309c1215f0474273816f4c2df8131ac6ceeda5
nehabalaji <nehabalaji10@gmail.com>
2020-09-29 22:54:41 +0530

    make changes to main activity

--- app/src/main/java/com/example/notesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/main/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.notesapp.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notesapp.R
 import com.example.notesapp.data.Notes
 import com.example.notesapp.ui.adapter.NotesPagingAdapter
 import com.example.notesapp.ui.adapter.NotesViewHolder
 import com.example.notesapp.ui.add.AddActivity
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -44,5 +48,49 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.noteslist.observe(this, Observer {
             notesPagingAdapter.submitList(it)
         })
+
+        val clickListener: NotesPagingAdapter.ClickListener = object : NotesPagingAdapter.ClickListener{
+            override fun onItemClick(position: Int, view: View) {
+                val currentNotes: Notes? = notesPagingAdapter.getNotesAtPosition(position)
+                if (currentNotes!=null){
+                    launchUpdateActivity(currentNotes)
+                }
+            }
+
+        }
+
+        val constraintLayout = constraint_layout
+        val snackbar = Snackbar.make(constraintLayout, "Notes Deleted", BaseTransientBottomBar.LENGTH_LONG)
+            .setAction("UNDO"){
+                mainViewModel.insertNotes(notes)
+            }
+
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position: Int = viewHolder.adapterPosition
+                notes = notesPagingAdapter.getNotesAtPosition(position)!!
+                mainViewModel.deleteNotes(notes)
+                snackbar.show()
+            }
+
+        })
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+        notesPagingAdapter.setItemClickListener(clickListener)
+    }
+
+    fun launchUpdateActivity(notes: Notes){
+        val intent = Intent(this, AddActivity::class.java)
+        intent.putExtra(extraDataNotesId, notes.id)
+        intent.putExtra(extraDataNotesTitle, notes.title)
+        intent.putExtra(extraDataNotesContent, notes.content)
+        startActivityForResult(intent, updateActivityCode)
     }
 }
